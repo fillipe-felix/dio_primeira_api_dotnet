8756f3223d73ea551504c30554a034b6342cde83
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-17 23:01:10 -0300

    alteração para que nos test não de erro

--- dio_primeira_api_dotnet/Models/Context.cs
+++ dio_primeira_api_dotnet/Models/Context.cs
@@ -4,8 +4,8 @@ namespace dio_primeira_api_dotnet.Models
 {
     public class Context : DbContext
     {
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Produto> Produtos { get; set; }
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Produto> Produtos { get; set; }
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
