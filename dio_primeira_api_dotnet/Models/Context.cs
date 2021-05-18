6d066083d03f5e1e02d0f0b9823e9c28424610ce
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-17 21:58:44 -0300

    - alteração para poder aparecer as Categorias quando eu busco os produtos

---
+++ dio_primeira_api_dotnet/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dio_primeira_api_dotnet.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=CursoMvc;Trusted_Connection=True;");
+        }
+    }
+}
